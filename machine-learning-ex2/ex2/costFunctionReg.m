997b1d9a19b75f219a6b1d780ec62b96c9e25102
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-08-04 20:38:15 -0300

    suffering a bit with the gradiaent calculation

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -30,10 +30,11 @@ Lambda(1)=0;
 % takes care to remove first element from theta, to avoid
 % unnecessary penalizing
 %thetaminus0=theta(2:end,1);
-J=1./m*( -y'*log(h)-(1-y)'*log(1-h) ) + 1./(2*m)*Lambda'*theta.^2;
+J=1/m*( -y'*log(h)-(1-y)'*log(1-h) ) + 1/(2*m)*Lambda'*theta.^2;
 
-grad=1./m*X'*(sigmoid(X*theta)-y) + 1./m*Lambda'.*theta;
-
+%grad=1/m*X'*(sigmoid(X*theta)-y) + 1/m*Lambda'.*theta; % for some reason this does not work!
+grad=1/m*X'*(sigmoid(X*theta)-y) + lambda/m*theta;
+grad(1)=grad(1)-lambda/m*theta(1);
 
 
 
