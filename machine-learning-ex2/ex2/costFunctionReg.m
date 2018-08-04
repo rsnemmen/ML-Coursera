ea4e601f0896d1c2870a5b255f06dfd596c63f8d
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-08-04 20:40:27 -0300

    fixed code

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -21,19 +21,12 @@ grad = zeros(size(theta));
 % m x 1 
 h=sigmoid(X*theta);
 
-% here is an elegant solution to use the same expressions for
-% all elements, including theta_0. Create a vector of lambdas,
-% where element 1 is zero
-Lambda=ones(size(theta))*lambda;
-Lambda(1)=0;
-
 % takes care to remove first element from theta, to avoid
 % unnecessary penalizing
-%thetaminus0=theta(2:end,1);
-J=1/m*( -y'*log(h)-(1-y)'*log(1-h) ) + 1/(2*m)*Lambda'*theta.^2;
+J=1/m*( -y'*log(h)-(1-y)'*log(1-h) ) + lambda/(2*m)*norm(theta(2:end,1))^2;
 
-%grad=1/m*X'*(sigmoid(X*theta)-y) + 1/m*Lambda'.*theta; % for some reason this does not work!
 grad=1/m*X'*(sigmoid(X*theta)-y) + lambda/m*theta;
+% corrects gradient for theta0
 grad(1)=grad(1)-lambda/m*theta(1);
 
 
