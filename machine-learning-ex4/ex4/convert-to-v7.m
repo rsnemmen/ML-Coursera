7d32183bc8d973247026eab383adc8e20f6eddf6
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2019-02-20 15:32:58 -0300

    generated data for hidden layer visualization

--- machine-learning-ex4/ex4/convert-to-v7.m
+++
@@ -1,9 +0,0 @@
-% Convert octave files to MATLAB's v7 format, such that I can open
-% them in python with scipy.io.loadmat. Otherwise, scipy.io gives 
-% me the error messages 
-%               ValueError: Unknown mat file type, version 35, 32
-
-
-maxiter=50;  % max number of iterations
-for i=1:maxiter,
-	
