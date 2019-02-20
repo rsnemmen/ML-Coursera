29944423ec6cf5f22cf648ae6dec94f2e9403c99
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2019-02-20 10:11:27 -0300

    prints accuracy in loop; saves array to disk

--- machine-learning-ex4/ex4/ex4.m
+++ machine-learning-ex4/ex4/ex4.m
@@ -181,57 +181,61 @@ pause;
 %
 fprintf('\nTraining Neural Network... \n')
 
-%  After you have completed the assignment, change the MaxIter to a larger
-%  value to see how more training helps.
-options = optimset('MaxIter', 10);
+% in order to learn what happens with the NN as the number of 
+% iterations increases, I put this little loop that dumps a
+% new image of Theta1 everytime one iteration of training is
+% finished
+maxiter=50;  % max number of iterations
+for i=1:maxiter,
+	%  After you have completed the assignment, change the MaxIter to a larger
+	%  value to see how more training helps.
+	options = optimset('MaxIter', i);
 
-%  You should also try different values of lambda
-lambda = 1;
+	%  You should also try different values of lambda
+	lambda = 1;
 
-% Create "short hand" for the cost function to be minimized
-costFunction = @(p) nnCostFunction(p, ...
-                                   input_layer_size, ...
-                                   hidden_layer_size, ...
-                                   num_labels, X, y, lambda);
+	% Create "short hand" for the cost function to be minimized
+	costFunction = @(p) nnCostFunction(p, ...
+	                                 input_layer_size, ...
+	                                 hidden_layer_size, ...
+	                                 num_labels, X, y, lambda);
 
-% Now, costFunction is a function that takes in only one argument (the
-% neural network parameters)
-[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+	% Now, costFunction is a function that takes in only one argument (the
+	% neural network parameters)
+	[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
 
-% Obtain Theta1 and Theta2 back from nn_params
-Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
+	% Obtain Theta1 and Theta2 back from nn_params
+	Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+	               hidden_layer_size, (input_layer_size + 1));
 
-Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+	Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+	               num_labels, (hidden_layer_size + 1));
 
 
-%% ================= Part 9: Visualize Weights =================
-%  You can now "visualize" what the neural network is learning by 
-%  displaying the hidden units to see what features they are capturing in 
-%  the data.
 
-fprintf('\nVisualizing Neural Network... \n')
+	%% ================= Part 9: Visualize Weights =================
+	%  You can now "visualize" what the neural network is learning by 
+	%  displaying the hidden units to see what features they are capturing in 
+	%  the data.
 
-[h,weights_arr]=displayData(Theta1(:, 2:end));
+	[h,weights_arr]=displayData(Theta1(:, 2:end));
 
-% save weights image to disk
-imwrite(weights_arr,"weights.png");
+	% save weights image to disk
+	imwrite(weights_arr,["weights" int2str(i) ".png"]);
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+	% save weights array to disk
+	save(["weights" int2str(i) ".mat"], "weights_arr");
 
-%% ================= Part 10: Implement Predict =================
-%  After training the neural network, we would like to use it to predict
-%  the labels. You will now implement the "predict" function to use the
-%  neural network to predict the labels of the training set. This lets
-%  you compute the training set accuracy.
 
-pred = predict(Theta1, Theta2, X);
+	%% ================= Part 10: Implement Predict =================
+	%  After training the neural network, we would like to use it to predict
+	%  the labels. You will now implement the "predict" function to use the
+	%  neural network to predict the labels of the training set. This lets
+	%  you compute the training set accuracy.
 
-fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+	pred = predict(Theta1, Theta2, X);
 
+	fprintf('  Accuracy: %f\n', mean(double(pred == y)) * 100);
+
+end;
 
