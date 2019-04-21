f777d3a573504ff9108e967dd84d93f205508b53
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2019-04-21 17:30:56 -0300

    best values of C, sigma

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -1,4 +1,4 @@
-function [C, sigma, errorVal] = dataset3Params(X, y, Xval, yval)
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
--- machine-learning-ex6/ex6/ex6.m
+++ machine-learning-ex6/ex6/ex6.m
@@ -139,7 +139,10 @@ pause;
 load('ex6data3.mat');
 
 % Try different SVM Parameters here
-[C, sigma] = dataset3Params(X, y, Xval, yval);
+%[C, sigma] = dataset3Params(X, y, Xval, yval);
+
+C=0.01;
+sigma=0.06;
 
 % Train the SVM
 model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
