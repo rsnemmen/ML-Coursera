8b14e9cf129548a232ced7c17a6dcfeb2ac6b5c7
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2019-04-21 17:20:09 -0300

    improved code, showing error bar and minimum-error-finder

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -1,4 +1,4 @@
-function [C, sigma] = dataset3Params(X, y, Xval, yval)
+function [C, sigma, errorVal] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
@@ -27,15 +27,19 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 % ============================
 
 % grid of C and sigma
-nmodels=3; %10	% number of models to be evaluated
+nmodels=10;	% number of models to be evaluated
 Cgrid=logspace(-2,1.5,nmodels);
 sigmaGrid=Cgrid;
 
+% number of values in Cgrid
+nC=numel(Cgrid);
+% number of values in sigmaGrid
+nsig=numel(sigmaGrid);
 % total model fits
-n=numel(Cgrid)*numel(sigmaGrid);
+n=nC*nsig;
 
 % array of CV errors
-errorVal=zeros();
+errorVal=zeros(nC,nsig);
 
 
 % LOOP OVER C AND SIGMA
@@ -43,10 +47,12 @@ errorVal=zeros();
 counter=0;
 for i=1:numel(Cgrid),
 	for j=1:numel(sigmaGrid),
-		w=waitbar(counter/n, "Model fitting, C=");
+		counter=counter+1;
 
 		C=Cgrid(i);
 		sigma=sigmaGrid(j);
+		% progress bar
+		w=waitbar(counter/n, ["Model fitting, C=",num2str(C)," sigma=",num2str(sigma)]);
 
 		% fit model to training data
 		model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
@@ -56,18 +62,21 @@ for i=1:numel(Cgrid),
 		predictions = svmPredict(model, Xval);
 
 		% 2. computes the prediction error using 
-		errorVal=mean(double(predictions ~= yval));
+		errorVal(i,j)=mean(double(predictions ~= yval));
 
-
-		%find C,sigma for which min(error_val)
-
-		counter=counter+1;
 		% erases progress bar
 		delete(w);
+	end;
 end;
 
+%find C,sigma which correspond to min(error_val)
+% ref: https://gist.github.com/Atlas7/6e03676da65bf467cb22d1ca42f20695
+[minval, imin] = min(min(errorVal,[],2));
+[minval, jmin] = min(min(errorVal,[],1));
+C=Cgrid(imin);
+sigma=sigmaGrid(jmin);
 
-
+fprintf('Best values: C=%f  sigma=%f\n', C, sigma);
 
 
 
