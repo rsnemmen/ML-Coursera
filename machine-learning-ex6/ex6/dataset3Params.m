07ae4c97504127eaf8ed21de058d35cdab9630d2
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2019-04-21 16:21:17 -0300

    code almost ready

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+%C = 1;
+%sigma = 0.3;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,25 +23,55 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-grid of C and sigma
+% INITIALIZES RELEVANT ARRAYS
+% ============================
 
-loop over C and sigma
+% grid of C and sigma
+nmodels=3; %10	% number of models to be evaluated
+Cgrid=logspace(-2,1.5,nmodels);
+sigmaGrid=Cgrid;
 
-fit model to training data
+% total model fits
+n=numel(Cgrid)*numel(sigmaGrid);
 
-compute error val
-
-find C,sigma for which min(error_val)
-
-
+% array of CV errors
+errorVal=zeros();
 
 
+% LOOP OVER C AND SIGMA
+% =======================
+counter=0;
+for i=1:numel(Cgrid),
+	for j=1:numel(sigmaGrid),
+		w=waitbar(counter/n, "Model fitting, C=");
 
-% returns the predictions on the cross validation set.
-predictions = svmPredict(model, Xval);
+		C=Cgrid(i);
+		sigma=sigmaGrid(j);
 
-% compute the prediction error using 
-mean(double(predictions ~= yval));
+		% fit model to training data
+		model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+
+		% compute error from CV set:
+		% 1. returns the predictions on the cross validation set
+		predictions = svmPredict(model, Xval);
+
+		% 2. computes the prediction error using 
+		errorVal=mean(double(predictions ~= yval));
+
+
+		%find C,sigma for which min(error_val)
+
+		counter=counter+1;
+		% erases progress bar
+		delete(w);
+end;
+
+
+
+
+
+
+
 
 
 
